696c6aadf96d1118933f36f9f1e4220ad1660f93
Andika Andriana <andriana.mia1@gmail.com>
2019-12-07 20:52:20 +0700

    Tugas 18 Javascript
    Tugas ini Saya kumpulkan untuk Niomic.

---
+++ tugas18_1.js
@@ -0,0 +1,7 @@
+function tugas18_1() {
+  var kalimat = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+
+  console.log(kalimat.slice(8, 24));
+}
+
+tugas18_1()
